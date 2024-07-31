4ce1d26450e2841a50426387d1f48313ae6d96db
Brian Perrett <bperrett@fusionbox.com>
2024-07-31 15:48:00 -0700

    Turn off virtual text

--- init.lua
+++ init.lua
@@ -333,6 +333,7 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open float
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 vim.diagnostic.config({
+  virtual_text = false,
   float = {
     source = true,
     border = 'rounded',
