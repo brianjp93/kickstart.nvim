2df9a1f5209e3eb8532c8e8086221a99dade0ad6
Brian Perrett <bperrett@fusionbox.com>
2025-08-27 23:11:14 -0700

    Update plugins

--- lua/custom/plugins/dadbod-ui.lua
+++ lua/custom/plugins/dadbod-ui.lua
@@ -1,29 +1,34 @@
-local cmp = require('cmp')
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = {"sql", "mysql", "plsql"},
-    callback = function()
-        cmp.setup.buffer {
-            sources = {
-                { name = 'vim-dadbod-completion' }
-            }
-        }
-    end,
-})
 return {
-  "kristijanhusak/vim-dadbod-ui",
-  dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
   },
-  cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
-  },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-  end,
+  { -- optional saghen/blink.cmp completion source
+    'saghen/blink.cmp',
+    opts = {
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        per_filetype = {
+          sql = { 'snippets', 'dadbod', 'buffer' },
+        },
+        -- add vim-dadbod-completion to your completion providers
+        providers = {
+          dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+        },
+      },
+    },
+  }
 }
