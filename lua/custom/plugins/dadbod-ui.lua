577988fbae8a8aa01a6132dbe70d0aa3dd65bbdc
Brian Perrett <perrettbrian@gmail.com>
2025-10-02 20:19:38 -0700

    Alter DBUI config

--- lua/custom/plugins/dadbod-ui.lua
+++ lua/custom/plugins/dadbod-ui.lua
@@ -14,15 +14,17 @@ return {
     init = function()
       -- Your DBUI configuration
       vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_execute_on_save = 0
+      vim.g.db_ui_auto_execute_table_helpers = 1
     end,
   },
   { -- optional saghen/blink.cmp completion source
     'saghen/blink.cmp',
     opts = {
       sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
+        default = { "lsp", "path", "buffer" },
         per_filetype = {
-          sql = { 'snippets', 'dadbod', 'buffer' },
+          sql = { 'lsp', 'snippets', 'dadbod', 'buffer' },
         },
         -- add vim-dadbod-completion to your completion providers
         providers = {
