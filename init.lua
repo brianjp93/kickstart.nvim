cd1596349c7fcd383ae5ac0aa9c96dfe98ec0315
Brian Perrett <perrettbrian@gmail.com>
2025-10-02 20:24:35 -0700

    not useful?

--- init.lua
+++ init.lua
@@ -109,9 +109,6 @@ require('lazy').setup({
         ['<Tab>'] = { 'select_next', 'fallback' },
         ['<S-Tab>'] = { 'select_prev', 'fallback' },
       },
-      appearance = {
-        nerd_font_variant = 'mono',
-      },
       completion = {
         documentation = {
           auto_show = true,
