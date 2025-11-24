af9651cbecc2e9b75ede012e4293f1a576f8d59d
Brian Perrett <bperrett@fusionbox.com>
2025-11-24 12:45:54 -0800

    Allow larger file.

--- init.lua
+++ init.lua
@@ -361,9 +361,10 @@ vim.defer_fn(function()
     highlight = {
       enable = true,
       disable = function(lang, buf)
-        local max_filesize = 100 * 1024
+        local max_filesize = 1000 * 1024
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
+          vim.print("Highlighting disabled due to file size.")
           return true
         end
       end
