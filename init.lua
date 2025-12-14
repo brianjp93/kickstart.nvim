466f0e100ac8d719e4acc3babadd55ce7f2d5434
Brian Perrett <perrettbrian@gmail.com>
2025-12-14 13:38:00 -0800

    Turn off preselection

--- init.lua
+++ init.lua
@@ -60,6 +60,13 @@ require('lazy').setup({
         ['<S-Tab>'] = { 'select_prev', 'fallback' },
       },
       completion = {
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = true,
+          },
+        },
+        ghost_text = { enabled = true },
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 100,
